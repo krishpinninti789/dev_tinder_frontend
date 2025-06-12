c4e8ecb25933552c09a97330d6bcca3988d1bfd2
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2025-06-12 19:35:43 +0530

    login api call

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return <div>Login page</div>;
+  const handleLogin = async () => {
+    const User = await axios.post(
+      "http://localhost:3000/login",
+      {
+        email,
+        password,
+      },
+      { withCredentials: true }
+    );
+  };
+
+  const [email, setEmail] = useState("kumar@gmail.com");
+  const [password, setPassword] = useState("Kumar@123");
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="card bg-black w-96 shadow-sm">
+        <div className="card-body flex flex-col">
+          <h2 className="card-title justify-center text-2xl">Login</h2>
+          <h1>Email Id</h1>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="mail@site.com"
+              required
+            />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
+          <h1>Password</h1>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              required
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              minlength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+            />
+          </label>
+          <p className="validator-hint hidden">
+            Must be more than 8 characters, including
+            <br />
+            At least one number <br />
+            At least one lowercase letter <br />
+            At least one uppercase letter
+          </p>
+          <div className="card-actions justify-end"></div>
+          <button
+            className="bg-gray-400 cursor-pointer text-black rounded-md p-3"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
